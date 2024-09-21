409d2cf1e8a1ab0b0dac3d4ae37f655411f86e61
Maik Schneider <mas@xima.de>
2024-09-21 11:55:56 +0200

    feat: set minimum php version to 7.0.33, adjust build script

--- bin/build.php
+++ bin/build.php
@@ -17,12 +17,11 @@ passthru('
 rm -rf build && mkdir build &&
 cp -r bin/ src/ composer.json LICENSE build/ && rm build/bin/build.php build/src/Box/Extract.php &&
 sed -i \'s/@dev/' . $version .'/g\' build/src/App.php &&
-composer config -d build/ platform.php 5.3.6 &&
+composer config -d build/ platform.php 7.0.33 &&
 composer install -d build/ --no-dev &&
 
 cd build/vendor && rm -rf */*/tests/ */*/src/tests/ */*/docs/ */*/*.md */*/composer.* */*/phpunit.* */*/.gitignore */*/.*.yml */*/*.xml && cd - >/dev/null &&
 cd build/vendor/symfony/ && rm -rf */Symfony/Component/*/Tests/ */Symfony/Component/*/*.md */Symfony/Component/*/composer.* */Symfony/Component/*/phpunit.* */Symfony/Component/*/.gitignore && cd ->/dev/null &&
-cd build/vendor/guzzle/guzzle && rm -r phar-stub.php phing/ && cd ->/dev/null &&
 build/bin/phar-composer build build/ ' . escapeshellarg($out) . ' &&
 
 echo -n "Reported version is: " && php ' . escapeshellarg($out) . ' --version', $code);
